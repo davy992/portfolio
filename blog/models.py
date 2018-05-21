63594445b66b08fe4ee78a45be0979c75834e1c0
Unknown <gnamus.david@gmail.com>
2018-05-21 14:19:26 +0200

    21.5.2018
    Ustvaril blog model po principu jobs modela

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    naslov = models.CharField(max_length = 30)
+    datum = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    besedilo = models.CharField(max_length = 2000)
