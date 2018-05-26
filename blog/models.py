89be642b5cd0f18ab544fd79941d09e1cff383d8
Unknown <gnamus.david@gmail.com>
2018-05-26 13:16:33 +0200

    26.5.2018 - končano!
    Dokončana spletna stran.

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,10 @@ class Blog(models.Model):
     naslov = models.CharField(max_length = 30)
     datum = models.DateField()
     image = models.ImageField(upload_to='images/')
-    besedilo = models.CharField(max_length = 2000)
+    besedilo = models.TextField(max_length = 2000)
+
+    def __str__(self):
+        return self.naslov
+
+    def povzetek(self):
+        return self.besedilo[:100]
